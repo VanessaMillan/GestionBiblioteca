6f6845036bf6b434a541bb3f4f309b6e5c170f40
VanessaMillan <130193230+VanessaMillan@users.noreply.github.com>
2025-05-03 18:18:08 -0500

    Delete BibliotecaWeb directory

--- BibliotecaWeb/Models/Dtos/VMUsuarioLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BibliotecaWeb.Models.Dtos
-{
-    public class VMUsuarioLogin
-    {
-        public string NombreUsuario { get; set; } = null!;
-        public string Clave { get; set; } = null!;
-    }
-}
